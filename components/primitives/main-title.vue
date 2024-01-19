443ee2b9cb460e2385d103e441318470d566e411
Alejandro Díaz <alejandrodiazpugh@gmail.com>
2024-01-18 19:35:08 -0600

    working title

---
+++ components/primitives/main-title.vue
@@ -0,0 +1,12 @@
+<template>
+	<h2 class="title">
+		<slot />
+	</h2>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+.title {
+	font-size: 28px;
+	color: white;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<h1>PLANNERSVP</h1>
+</template>
+<script setup lang="ts"></script>
+<style scoped>
+body {
+	font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI',
+		Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue',
+		sans-serif;
+}
+</style>
