6e74b2c6d39322e2148fea0ca1f679740b11a653
Alejandro Diaz <alejandrodiazpugh@gmail.com>
2024-01-25 14:00:57 -0600

    basic footer

--- components/global/footer.vue
+++ components/global/footer.vue
@@ -1,7 +1,15 @@
 <template>
-	<footer>
-		<h3>PlanneRSVP</h3>
-	</footer>
+    <footer class="footer">
+        <h3>PlanneRSVP</h3>
+        <p>By diaz pugh</p>
+    </footer>
 </template>
 <script setup lang="ts"></script>
-<style scoped></style>
+<style scoped>
+.footer {
+    padding-top: 20px;
+    min-height: 100px;
+    box-shadow: 0px -5px 30px var(--dark-200);
+    text-align: center;
+}
+</style>
